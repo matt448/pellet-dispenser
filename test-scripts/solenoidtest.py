9487bd977f02ab28a18716828d737aff129b7595
matt448 <matthew.mcmillan@gmail.com>
2013-03-31 20:40:46 -0500

    Solenoid test script written by Mason

---
+++ test-scripts/solenoidtest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Author: Mason McMillan
+
+import pymcu
+from time import sleep
+mb =pymcu.mcuModule()
+i = 0
+
+while i < 20:
+    i = i + 1
+    mb.pinToggle(11)
+    sleep(1)
+
